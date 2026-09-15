b621382c97f96fea44ecb71e6b228bc9a198f54c
conectfono <contagamer8292@gmail.com>
2026-09-15 11:09:05 -0300

    Delete assets/supabase-config.js

--- assets/supabase-config.js
+++
@@ -1,2 +0,0 @@
-document.documentElement.dataset.supabaseUrl = 'https://drwtzwcgkqwealesdutq.supabase.co/rest/v1/';
-document.documentElement.dataset.supabaseKey = 'sb_publishable_ULtSY8GdvicDrtcDBamJfw__x51Pyg1';
