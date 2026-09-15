4de28a0625f87910c82caa12f9ba909e099e7403
conectfono <contagamer8292@gmail.com>
2026-09-15 11:10:27 -0300

    Add Supabase configuration to supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,2 @@
+document.documentElement.dataset.supabaseUrl = 'https://drwtzwcgkqwealesdutq.supabase.co/rest/v1/';
+document.documentElement.dataset.supabaseKey = 'sb_publishable_ULtSY8GdvicDrtcDBamJfw__x51Pyg1';
